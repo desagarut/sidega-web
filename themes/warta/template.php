57fda5c54af385e615e9b408a91b488bdb29f255
Desa Garut <desagarut.net@gmail.com>
2023-03-30 23:37:06 +0700

    Fix Tema Warta

--- themes/warta/template.php
+++ themes/warta/template.php
@@ -6,10 +6,9 @@
     <?php $this->load->view($folder_themes . '/commons/spinner.php') ?>
     <?php $this->load->view($folder_themes . '/commons/header.php') ?>
     <main>
-    <?php $this->load->view($folder_themes . '/layouts/home.tpl.php') ?>
+        <?php $this->load->view($folder_themes . '/layouts/home.tpl.php') ?>
     </main>
-<?php $this->load->view($folder_themes . '/commons/footer') ?>
-
+    <?php $this->load->view($folder_themes . '/commons/footer') ?>
 </body>
 
 </html>
--- themes/warta/widgets/running_text.php
+++ themes/warta/widgets/running_text.php
@@ -1,15 +1,18 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed'); ?>
 
 <?php if ($teks_berjalan) : ?>
-
-  <?php foreach ($teks_berjalan as $teks) : ?>
-    <a href="<?= $teks['tautan'] ?>" title="Baca Selengkapnya">
-      <marquee onmouseover="this.stop()" onmouseout="this.start()">
-        <?= $teks['teks'] ?>
-        <?php if ($teks['tautan']) : ?>
-          <?= $teks['judul_tautan'] ?>
-        <?php endif; ?>
-      </marquee>
-    </a>
-  <?php endforeach; ?>
+  <div class="row">
+    <marquee onmouseover="this.stop()" onmouseout="this.start()">
+      <?php foreach ($teks_berjalan as $teks) : ?>
+        <a href="<?= $teks['tautan'] ?>" target="_blank">
+          <span class="teks">
+            <?= $teks['teks'] ?>
+            <?php if ($teks['tautan']) : ?>
+              <?= $teks['judul_tautan'] ?>
+            <?php endif; ?>
+          </span>
+        </a>&nbsp;|&nbsp;
+      <?php endforeach; ?>
+    </marquee>
+  </div>
 <?php endif; ?>
