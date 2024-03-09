850c5d59fd276bc98506560168141bd4ed3b2359
Desa Garut <desagarut.net@gmail.com>
2024-03-09 11:39:47 +0700

    Ubah layout Front, Hapus file tidak dibutuhkan

--- themes/warta/layouts/home.tpl.php
+++ themes/warta/layouts/home.tpl.php
@@ -1,15 +1,14 @@
-<?php if(!defined('BASEPATH')) exit('No direct script access allowed'); ?>
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed'); ?>
 
-<?php $this->load->view($folder_themes .'/partials/weekly') ?>
-<?php $this->load->view($folder_themes .'/partials/carousel') ?>
+<?php $this->load->view($folder_themes . '/partials/carousel') ?>
 <!-- List Konten -->
-<?php $title = ( ! empty($judul_kategori)) ? $judul_kategori['kategori'] : "Artikel Terkini"; ?>
-<?php $this->load->view($folder_themes .'/partials/trending') ?>
+<?php $title = (!empty($judul_kategori)) ? $judul_kategori['kategori'] : "Artikel Terkini"; ?>
+<?php $this->load->view($folder_themes . '/partials/weekly') ?>
 
-<?php if ($headline): ?>
-<?php $this->load->view($folder_themes .'/partials/whatnew') ?>
+<?php if ($headline) : ?>
+    <?php $this->load->view($folder_themes . '/partials/whatnew') ?>
 <?php endif; ?>
 
-<?php $this->load->view($folder_themes .'/partials/most-popular') ?>
-<?php $this->load->view($folder_themes .'/partials/video') ?>
-<?php $this->load->view($folder_themes .'/widgets/banner-one') ?>
+<?php $this->load->view($folder_themes . '/partials/most-popular') ?>
+<?php $this->load->view($folder_themes . '/partials/video') ?>
+<?php $this->load->view($folder_themes . '/widgets/banner-one') ?>
