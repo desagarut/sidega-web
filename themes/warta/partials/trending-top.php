ef2614e46d25296fa140959bc5fdc9a1328aa74a
Desa Garut <desagarut.net@gmail.com>
2024-03-11 18:23:57 +0700

    Fixbugs - Trending-top Perisai BPJS Ketenagakerjaan

--- themes/warta/partials/trending-top.php
+++ themes/warta/partials/trending-top.php
@@ -7,8 +7,8 @@
                                     <?php if (!defined('BASEPATH')) exit('No direct script access allowed'); ?>
                                     <?php $abstract = potong_teks($headline['isi'], 150); ?>
                                     <?php $url = site_url('artikel/' . buat_slug($headline)); ?>
-                                    <?php $image = ($headline['gambar'] && is_file(LOKASI_FOTO_ARTIKEL . 'kecil_' . $headline['gambar'])) ?
-                                        AmbilFotoArtikel($headline['gambar'], 'kecil') :
+                                    <?php $image = ($headline['gambar'] && is_file(LOKASI_FOTO_ARTIKEL . 'sedang_' . $headline['gambar'])) ?
+                                        AmbilFotoArtikel($headline['gambar'], 'sedang') :
                                         base_url($this->theme_folder . '/' . $this->theme . '/assets/images/placeholder.png') ?>
 
 
@@ -26,11 +26,10 @@
                         <div class="col-lg-12 col-md-6 col-sm-6">
                             <div class="trending-top mb-30 d-flex wow fadeInUp" data-wow-delay="0.7s">
                                 <div class="trend-top-img">
-                                    <img src="<?= base_url("$this->theme_folder/$this->theme/assets/img/trending/trending_top4.jpg") ?>" alt="">
+                                    <img src="<?= base_url("$this->theme_folder/$this->theme/assets/img/perisai bpjs ketenagakerjaan.png") ?>" alt="">
                                     <div class="trend-top-cap trend-top-cap2">
-                                        <span class="bgg">TECH </span>
-                                        <h2><a href="latest_news.html">Secretart for Economic Air plane that looks like</a></h2>
-                                        <p>by Alice cloe - Jun 19, 2020</p>
+                                        <span class="bgg">Program Kerja </span>
+                                        <h2><a href="https://desagarut.id/artikel/2024/3/11/perisai-bpjs-ketenagakerjaan">Perisai BPJS Ketenagakerjaan</a></h2>
                                     </div>
                                 </div>
                             </div>
