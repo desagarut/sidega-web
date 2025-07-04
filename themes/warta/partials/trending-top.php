51e8e3c08176ff62ba81679a42d7d28031399c4f
Desa Garut <desagarut.net@gmail.com>
2025-07-04 10:38:31 +0700

    Update Icon
    trending-top

--- themes/warta/partials/trending-top.php
+++ themes/warta/partials/trending-top.php
@@ -1,37 +1,62 @@
-                    <!-- Trending Top -->
-                    <div class="row">
-                        <div class="col-lg-12 col-md-6 col-sm-6">
-                            <div class="trending-top mb-30 d-flex wow fadeInUp" data-wow-delay="0.2s">
-                                <div class="trend-top-img">
-
-                                    <?php if (!defined('BASEPATH')) exit('No direct script access allowed'); ?>
-                                    <?php $abstract = potong_teks($headline['isi'], 150); ?>
-                                    <?php $url = site_url('artikel/' . buat_slug($headline)); ?>
-                                    <?php $image = ($headline['gambar'] && is_file(LOKASI_FOTO_ARTIKEL . 'sedang_' . $headline['gambar'])) ?
-                                        AmbilFotoArtikel($headline['gambar'], 'sedang') :
-                                        base_url($this->theme_folder . '/' . $this->theme . '/assets/images/placeholder.png') ?>
-
-
-                                    <img src="<?= $image ?>" alt="<?= $headline['judul'] ?>">
-                                    <div class="trend-top-cap trend-top-cap2">
-                                        <span class="bgb"><?= $gambar['id_kategori'] ?></span>
-                                        <h2><a href="<?= $url; ?>"><?= $headline['judul'] ?></a></h2>
-                                        <p><?= $headline['owner'] ?> - <?= tgl_indo($headline['tgl_upload']); ?> - <?= hit($headline['hit']); ?></p>
-                                    </div>
-
-
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-12 col-md-6 col-sm-6">
-                            <div class="trending-top mb-30 d-flex wow fadeInUp" data-wow-delay="0.7s">
-                                <div class="trend-top-img">
-                                    <img src="<?= base_url("$this->theme_folder/$this->theme/assets/img/perisai bpjs ketenagakerjaan.png") ?>" alt="">
-                                    <div class="trend-top-cap trend-top-cap2">
-                                        <span class="bgg">Program Kerja </span>
-                                        <h2><a href="https://desagarut.id/artikel/2024/3/11/perisai-bpjs-ketenagakerjaan">Perisai BPJS Ketenagakerjaan</a></h2>
-                                    </div>
-                                </div>
-                            </div>
+<!-- Trending Top -->
+<div class="row">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-3 col-sm-3 col-xs-3">
+                <div class="trending-top mb-30 d-flex wow fadeInUp" data-wow-delay="0.7s">
+                    <div class="trend-top-img">
+                        <img src="<?= base_url("$this->theme_folder/$this->theme/assets/img/live_tv.png") ?>" alt="live tv">
+                        <div class="trend-top-cap trend-top-cap2">
+                            <span class="bgg">Hiburan</span>
+                            <h2><a href="<?= base_url("first/live_tv"); ?>">LIVE TV</a></h2>
                         </div>
                     </div>
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-3 col-sm-3 col-xs-3">
+                <div class="trending-top mb-30 d-flex wow fadeInUp" data-wow-delay="0.7s">
+                    <div class="trend-top-img">
+                        <img src="<?= base_url("$this->theme_folder/$this->theme/assets/img/cctv.fw.png") ?>" alt="cctv">
+                        <div class="trend-top-cap trend-top-cap2">
+                            <span class="bgg">Pantau</span>
+                            <h2><a href="<?= site_url("first/cctv"); ?>">CCTV</a></h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-3 col-sm-3 col-xs-3">
+                <div class="trending-top mb-30 d-flex wow fadeInUp" data-wow-delay="0.7s">
+                    <div class="trend-top-img">
+                        <img src="<?= base_url("$this->theme_folder/$this->theme/assets/img/youtube_1.png") ?>" alt="video youtube">
+                        <div class="trend-top-cap trend-top-cap2">
+                            <span class="bgg">Seputar Desa</span>
+                            <h2><a href="<?= site_url("first/gallery_youtube"); ?>">VIDEO</a></h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-3 col-sm-3 col-xs-3">
+                <div class="trending-top mb-30 d-flex wow fadeInUp" data-wow-delay="0.7s">
+                    <div class="trend-top-img">
+                        <img src="<?= base_url("$this->theme_folder/$this->theme/assets/img/foto.png") ?>" alt="Gallery Foto">
+                        <div class="trend-top-cap trend-top-cap2">
+                            <span class="bgg">Kegiatan</span>
+                            <h2><a href="<?= site_url("first/gallery"); ?>">FOTO</a></h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-12 col-md-12 col-sm-12 col-auto">
+        <div class="trending-top mb-30 d-flex wow fadeInUp" data-wow-delay="0.7s">
+            <div class="trend-top-img">
+                <img src="<?= base_url("$this->theme_folder/$this->theme/assets/img/tutorial_sidega.png") ?>" alt="live tv">
+                <div class="trend-top-cap trend-top-cap2">
+                    <span class="bgg">SIDeGa</span>
+                    <h2><a href="<?= site_url("artikel/2024/3/22/sidega-sistem-informasi-desa-garut"); ?>">Sistem Informasi Desa Garut</a></h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
